80aeae98ba9138f050c83b8e7cc19feaacc535ed
ArtengoClan <135038710+ArtengoClan@users.noreply.github.com>
2023-11-19 23:31:55 +0100

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: '/gitogram/dist/'
+// })
+module.exports = {
   publicPath: '/gitogram/dist/'
-})
+}
